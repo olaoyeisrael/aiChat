d5f4d410d2664cb5883c8e916a92816222357176
Israel <olaoyeaisrael@gmail.com>
2025-05-01 00:40:35 +0100

    setUp env

--- app/core/database.py
+++ app/core/database.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_SERVER')}"
-# SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_SERVER')}"
-SQLALCHEMY_DATABASE_URL = 'postgresql://ai_postgres_pp8q_user:Mi9yUR74AdPiUN480KF8bcqw84g6OxzH@dpg-d04v7q6uk2gs73e38ppg-a.oregon-postgres.render.com/ai_postgres_pp8q'
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_SERVER')}"
+# SQLALCHEMY_DATABASE_URL = 'postgresql://ai_postgres_pp8q_user:Mi9yUR74AdPiUN480KF8bcqw84g6OxzH@dpg-d04v7q6uk2gs73e38ppg-a.oregon-postgres.render.com/ai_postgres_pp8q'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
